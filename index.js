8fd4247af682f0098dde6fbe3331e7b9b4aced0d
S_Kaushik <116005168+Sanatan-Kaushik13@users.noreply.github.com>
2022-12-16 13:25:56 +0000

    modified:   index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 //This is proto
+class Dog{
+    constructor(_name){
+        this.name=_name
+        console.log(`The name is ${this.name}`)
+    }
+
+    sleep(){
+        console.log("The dog slept.")
+    }
+
+    static bark(){
+        console.log("BARK!")
+    }
+}
+
+let d=new Dog("Jim")
+d.sleep()
+console.log(d.name)
+Dog.bark()
